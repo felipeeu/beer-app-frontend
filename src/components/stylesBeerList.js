f6fe425efa3c610fe1b022da38b9fa32a5c3730a
Felipe Domingues <felipeeu@yahoo.com.br>
2019-12-02 10:15:32 -0300

    improving some styles

--- src/components/stylesBeerList.js
+++ src/components/stylesBeerList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BeerStylesCard} from '../styles/beerStyles'
+import {BeerStylesCard, Data} from '../styles/beerStyles'
 
 
 export const StylesList = ({styles}) => {
@@ -9,18 +9,20 @@ return(
     styles.map((style,idx)=>{
         return(
             <BeerStylesCard key= {idx}>
+            <Data>
             <span>
-            Estilo: {style.name}
+            <b>Estilo:</b> {style.name}
             </span>
             <span>
-            Graduação alcoólica: {style.abv}
+            <b>Graduação alcoólica:</b> {style.abv} %
             </span>
             <span>
-            Índice de amargor: {style.ibu}
+            <b>Índice de amargor:</b> {style.ibu} IBU
             </span>
             <span>
-            Harmonizações: {style.pair}
+            <b>Harmonizações:</b> {style.pair}
             </span>
+            </Data>
             <img src ={style.img} alt= 'beerimage'/>
             </BeerStylesCard>
         )
--- src/styles/beerStyles.js
+++ src/styles/beerStyles.js
@@ -15,3 +15,9 @@ export const BeerStylesCard= styled.div`
   border-radius: 3px;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 `;
+
+export const Data = styled.div`
+  display: grid;
+  
+
+`;
